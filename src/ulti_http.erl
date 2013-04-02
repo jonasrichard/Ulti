fd2e0276d6fc5d8900393b73f49bf2d73f2df559
Jonas, Richard <richard.jonas.76@gmail.com>
2013-04-02 21:52:10 +0200

    Simple cowboy server

--- src/ulti_http.erl
+++ src/ulti_http.erl
@@ -3,4 +3,12 @@
 -author("richard").
 
 %% API
--export([]).
+-export([start/0]).
+
+start() ->
+  application:start(crypto),
+  application:start(ranch),
+  application:start(cowboy),
+  Dispatch = cowboy_router:compile([{'_', [{"/", ulti_http_handler, []}]}]),
+  {ok, _} = cowboy:start_http(http, 100, [{port, 8080}],
+          [{env, [{dispatch, Dispatch}]}]).
--- src/ulti_http_handler.erl
+++ src/ulti_http_handler.erl
@@ -3,4 +3,16 @@
 -author("richard").
 
 %% API
--export([]).
+-export([init/3, handle/2, terminate/3]).
+
+init(_Transport, Req, []) ->
+  {ok, Req, undefined}.
+
+handle(Req, State) ->
+  {Method, Req2} = cowboy_req:method(Req),
+  {Echo, Req3} = cowboy_req:qs_val(<<"echo">>, Req2),
+  {ok, Req4} = cowboy_req:reply(200, [], "Echo: " ++ Echo, Req3),
+  {ok, Req4, State}.
+
+terminate(_Reason, _Req, _State) ->
+  ok.
